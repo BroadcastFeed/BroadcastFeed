a4eab770bf1b9a4c7ba6a20f60980a352026071b
ricco <riccovcs@terra.com.br>
2022-02-27 18:49:31 -0300

    Create empty class ProfileSessionManager

---
+++ server/src/ProfileSessionManager.cpp
@@ -0,0 +1,2 @@
+#include "ProfileSessionManager.h"
+
---
+++ server/src/ProfileSessionManager.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "State.h"
+#include "Profile.h"
+#include "Notification.h"
+
+class ProfileSessionManager{
+    private:
+        State database;
+
+    public:
+}
