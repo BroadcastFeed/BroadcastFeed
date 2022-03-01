996f348c036568b4b96aa08842fe87dd5460cec2
ricco <riccovcs@terra.com.br>
2022-03-01 19:24:56 -0300

    Add server ProfileSessionManager methods

--- server/src/ProfileSessionManager.cpp
+++ server/src/ProfileSessionManager.cpp
@@ -1,2 +1,16 @@
 #include "ProfileSessionManager.h"
 
+bool ProfileSessionManager::consultUsername(std::string username){
+    return true;
+    //not implemented
+}
+
+void ProfileSessionManager::addNewUser(std::string username){
+    this->database.addUser(username);
+}
+
+void ProfileSessionManager::login(std::string username){
+    if (!this->consultUsername(username)){
+        this->addNewUser(username);
+    }
+}
--- server/src/ProfileSessionManager.h
+++ server/src/ProfileSessionManager.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 #include "Database.h"
 #include "Profile.h"
 #include "Notification.h"
@@ -8,4 +9,7 @@ class ProfileSessionManager{
         Database database;
 
     public:
+        bool consultUsername(std::string username); //returns the existance or not of a give username
+        void addNewUser(std::string username);
+        void login(std::string username);
 };
